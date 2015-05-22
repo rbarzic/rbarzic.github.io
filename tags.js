170b5e95dc7493a103a3e58f56cd5d605aa34b03
Ronan Barzic <rbarzic@gmail.com>
2015-05-22 13:45:43 +0200

    New version of website using o-blog v2 - Content is not changed

---
+++ tags.js
@@ -0,0 +1,1 @@
+{"tags":[{"size":"150.00","path":"tags/bootstrap.html","tag":"Bootstrap"},{"size":"80.00","path":"tags/html-5.html","tag":"html 5"},{"size":"136.00","path":"tags/o-blog.html","tag":"o-blog"},{"size":"80.00","path":"tags/org.html","tag":"org"},{"size":"220.00","path":"tags/usage.html","tag":"usage"}]}
